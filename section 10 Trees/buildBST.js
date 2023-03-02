a64a4941b93f2d739631b1d9a564e0f050cc481f
Ian Hwang <meen813@hotmail.com>
2023-03-01 16:12:11 -0800

    binary search tree remove function implemented

--- section 10 Trees/buildBST.js
+++ section 10 Trees/buildBST.js
@@ -12,33 +12,33 @@ class BinarySearchTree {
     }
     insert(value) {
         const newNode = new Node(value);
-        if(this.root === null){
+        if (this.root === null) {
             this.root = newNode
             return this;
-        }else {
+        } else {
             let currentNode = this.root;
-            while(true){
-                if(value < currentNode.value){
+            while (true) {
+                if (value < currentNode.value) {
                     // go left
-                    if(!currentNode.left){
+                    if (!currentNode.left) {
                         currentNode.left = newNode;
                         return this;
                     }
                     currentNode = currentNode.left
-                }else {
+                } else {
                     // go Right
-                    if(!currentNode.right){
+                    if (!currentNode.right) {
                         currentNode.right = newNode;
                         return this;
                     }
                     currentNode = currentNode.right;
                 }
-            } 
+            }
         }
     }
-    
+
     lookup(value) {
-        if(!this.root){
+        if (!this.root) {
             return false;
         }
         let currentNode = this.root;
@@ -47,35 +47,82 @@ class BinarySearchTree {
                 currentNode = currentNode.left;
             } else if (value > currentNode.value) {
                 currentNode = currentNode.right;
-            } else if (currentNode.value === value){
+            } else if (currentNode.value === value) {
                 return currentNode;
             }
-        } 
+        }
         return false;
     }
-    remove(value){
+    remove(value) {
         // if the value of node does not exist?
-        let currentNode = this.root;
-        let prevNode = this.root;
-        if(this.root !== null){
-            let prevNode.right = currentNode;
+        if (this.root === null) {
+            return false;
         }
+        let currentNode = this.root;
+        let parentNode = null;
         while (currentNode) {
             if (value < currentNode.value) {
+                parentNode = currentNode;
                 currentNode = currentNode.left;
             } else if (value > currentNode.value) {
+                parentNode = currentNode;
                 currentNode = currentNode.right;
-            } else if (currentNode.value === value){
-                if(currentNode.right === null && currentNode.left === null){
-                    currentNode = null;
-                    return this;
+            } else if (currentNode.value === value) {
+                // Option 1: No right child:
+                if (currentNode.right === null) {
+                    if (parentNode === null) {
+                        this.root === currentNode.left;
+                    } else {
+                        // if parent > currnet value, make current left child a child of parent
+                        if (currentNode.value < parentNode.value) {
+                            parentNode.left === currentNode.left;
+                        }
+                        // if parent < current.value, make left child a right child of parent
+                        else if (currentNode.value > parentNode.value) {
+                            parentNode.right === currentNode.left;
+                        }
+                    }
                 }
-                if(currentNode.right === null && currentNode !== null){
-                    currentNode.right
+                // Option 2: Right child which doesnt have a left child
+                else if (currentNode.right.left === null) {
+                    if (parentNode === null) {
+                        this.root === currentNode.left;
+                    } else {
+                        currentNode.right.left = currentNode.left; //why do we need this?
+                        if (currentNode.value < parentNode.value) {
+                            parentNode.left = currentNode.right;
+                        }
+                        else if (currentNode.value > parentNode.value) {
+                            parentNode.right = currentNode.right;
+                        }
+                    }
+
+                }
+                // Option 3: right child that has a left child
+                else {
+                    //find the right child's left most child
+                    let leftmost = currentNode.right.left;
+                    let leftmostParent = currentNode.right;
+                    while (leftmost.left !== null) {
+                        leftmostParent = leftmost;
+                        leftmost = leftmost.left;
+                    }
+                    //parent's left subtree is now leftmost's right subtree
+                    leftmostParent.left = leftmost.right;
+                    leftmost.left = currentNode.left;
+                    leftmost.right = currentNode.right;
+                    if (parentNode === null) {
+                        this.root = leftmost;
+                    } else {
+                        if (currentNode.value < parentNode.value) {
+                            parentNode.left = leftmost;
+                        } else if (currentNode.value > parentNode.value) {
+                            parentNode.right = leftmost;
+                        }
+                    }
                 }
             }
-        } 
-
+        }
     }
 }
 
