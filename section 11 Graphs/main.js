e14615869405d64cd494b8709d8a88b777916621
Ian Hwang <meen813@hotmail.com>
2023-03-05 22:50:17 -0800

    graphs

---
+++ section 11 Graphs/main.js
@@ -0,0 +1,15 @@
+//Edge list
+const graph = [[0, 2], [2, 3], [2, 1], [1, 3]]
+
+//Adjacent List
+//The index of the array is the value of the actual Graph node
+const graph2 = [[2], [2,3], [0,1,3], [1,2]]
+
+
+//Adjacent Matrix
+const graph3 = {
+    1: [0, 0, 1, 0],
+    2: [0, 0, 1, 1],
+    3: [1, 1, 0, 1],
+    4: [0, 1, 1, 0]
+}
