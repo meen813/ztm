fb383b7e22cf411dfe0f469325a3df3ba988ac48
Ian Hwang <meen813@hotmail.com>
2023-03-08 22:58:09 -0800

    selection sorting

---
+++ section 13 Sorting/selectionSort.js
@@ -0,0 +1,40 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function selectionSort(array) {
+    const length = array.length;
+    let temp;
+    for(let i = 0; i < length-1; i++){
+        for(let j = i+1; j < length; j++){
+            if(array[i] > array[j]){
+                temp = array[i];
+                array[i] = array[j]
+                array[j] = temp
+            }
+        }
+    }
+    return array;
+}
+
+console.log(selectionSort(numbers)) 
+
+// By Andre
+// function selectionSort(array) {
+//     const length = array.length;
+//     for (let i = 0; i < length; i++) {
+//         // set current index as minimum
+//         let min = i;
+//         let temp = array[i];
+//         for (let j = i + 1; j < length; j++) {
+//             if (array[j] < array[min]) {
+//                 //update minimum if current is lower that what we had previously
+//                 min = j;
+//             }
+//         }
+//         array[i] = array[min];
+//         array[min] = temp;
+//     }
+//     return array;
+// }
+
+// selectionSort(numbers);
+// console.log(numbers);
