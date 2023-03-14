dcbc8e9224da80e976b01371b4c914cf09ec277c
Ian Hwang <meen813@hotmail.com>
2023-03-13 20:45:14 -0700

    sorting finished

--- section 13 Sorting/sortingInterview.js
+++ section 13 Sorting/sortingInterview.js
@@ -1,15 +1,28 @@
 //#1 - Sort 10 schools around your house by distance:
+    //insertion sort: small size of data, O(1) space complexity.
 
 //#2 - eBay sorts listings by the current Bid amount:
+    // radix or counting sort: numbers with a certain range.
+
 
 //#3 - Sport scores on ESPN
+    //Quick sort: better space complexity.
 
 //#4 - Massive database (can't fit all into memory) needs to sort through past year's user data
+    // merge sort: large date sets
 
 //#5 - Almost sorted Udemy review data needs to update and add 2 new reviews
-
+    //insertion sort : small data sets & nearly sorted
+    
 //#6 - Temperature Records for the past 50 years in Canada
+    // quick sort: better space complexity.
 
 //#7 - Large user name database needs to be sorted. Data is very random.
+    // merge sort: large date sets
 
 //#8 - You want to teach sorting for the first time
+    // Bubble sort or Selection sort
+
+
+    // enough memory = merge sort?
+    // limited memory = quick sory
