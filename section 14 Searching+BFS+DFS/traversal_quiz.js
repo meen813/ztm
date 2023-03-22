eabdee24ea5f28767f03b617aa630368be40e154
Ian Hwang <meen813@hotmail.com>
2023-03-22 03:45:03 -0700

    DFS implementation 1

---
+++ section 14 Searching+BFS+DFS/traversal_quiz.js
@@ -0,0 +1,17 @@
+//If you know a solution is not far from the root of the tree:
+    //BFS: it starts where close to the parent
+
+//If the tree is very deep and solutions are rare: 
+    //BFS: because DFS will take long.
+
+//If the tree is very wide:
+    //DFS: BFS will need too much memory
+
+//If solutions are frequent but located deep in the tree:
+    //DFS   
+
+//Determining whether a path exists between two nodes:
+    //DFS
+
+//Finding the shortest path:
+    //BFS
